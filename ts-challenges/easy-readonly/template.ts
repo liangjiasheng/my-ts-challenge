ca8ad422306e8b470dc43be8d3419e17f8e4dfdb
liangjiasheng <845061953@qq.com>
2022-07-05 23:38:10 +0800

    feat: easy-readonly

--- ts-challenges/easy-readonly/template.ts
+++ ts-challenges/easy-readonly/template.ts
@@ -4,6 +4,19 @@ type MyReadonly<T> = {
 
 // js 的实现思路
 // function MyReadonly(raw) {
+// 思路1
+// 1、创建一个新对象
+// 2、遍历对象 mapped -> in 关键字；lookup -> keyof 关键字
+// 3、给每个 key 加上 readonly 关键字
+// 4、把 raw[key] 的值赋给新对象
+// 5、返回新对象
+// 	const result = {};
+// 	for (const key of raw) {
+// 		result[`readonly ${key}`] = raw[key];
+// 	}
+// 	return result;
+
+// 思路2
 // 	return new Proxy(raw, {
 // 		get(target, key) {
 // 			return Reflect.get(target, key);
